4e25e5387fda3484350c154ade943a477a1fc950
Loki Astari <Loki.Astari@gmail.com>
2023-07-04 15:26:24 -0700

    Update header-only

---
+++ GitUtility/GitUtilityConfig.h
@@ -0,0 +1,13 @@
+/* src/GitUtility/GitUtilityConfig.h.  Generated from GitUtilityConfig.h.in by configure.  */
+/* src/GitUtility/GitUtilityConfig.h.in.  Generated from configure.ac by autoheader.  */
+
+#ifndef THORS_GITUTILITY_CONFIG_H
+#define THORS_GITUTILITY_CONFIG_H
+
+/* Enable to use header only libraries */
+#define HEADER_ONLY 1
+
+/* For header only convery to inline */
+#define HEADER_ONLY_INCLUDE inline
+
+#endif
--- ThorsStorage/test/makedependency/unittest.d
+++
@@ -1,2 +0,0 @@
-makedependency/unittest.d debug/unittest.o release/unittest.o \
-  coverage/unittest.o: unittest.cpp
