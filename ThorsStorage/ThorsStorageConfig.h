e12b8e38c3499d5b75f7306ef67a293ff74835ca
Loki Astari <Loki.Astari@gmail.com>
2023-07-23 10:15:38 -0700

    Update with latest fiexes

--- ThorsStorage/ThorsStorageConfig.h
+++ ThorsStorage/ThorsStorageConfig.h
@@ -9,5 +9,8 @@
 /* For header only convery to inline */
 #define HEADER_ONLY_INCLUDE inline
 
+/* Disable test that require files to be locked */
+#define THOR_DISABLE_TEST_WITH_LOCKED_FILES 0
+
 #endif
 
