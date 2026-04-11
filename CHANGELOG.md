af011d8102a76161f2d2b39bd2bb2f5690061566
Loki Astari <Loki.Astari@gmail.com>
2026-04-11 00:51:01 -0700

    Update all standard files

---
+++ CHANGELOG.md
@@ -0,0 +1,67 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/), and this project adheres to [Semantic Versioning](https://semver.org/).
+
+## [9.1.11] - 2026-04-11
+
+### Changed
+- License changed from GPL-3.0 to MIT
+- Updated Conan recipe with correct dependencies and package info
+
+## [9.1.7] - 2026-03-22
+
+### Changed
+- Various updates and improvements
+
+## [9.1.5] - 2026-03-09
+
+### Fixed
+- Applied multiple code fixes
+
+## [9.1.4] - 2026-03-04
+
+### Fixed
+- Fixed Windows build
+
+## [9.1.3] - 2026-03-04
+
+### Fixed
+- Fixed Homebrew build issue
+
+## [9.1.2] - 2026-03-03
+
+### Fixed
+- Various fixes and updates
+
+## [9.0.16] - 2026-01-23
+
+### Changed
+- Updated to latest version
+
+## [9.0.11] - 2026-01-18
+
+### Fixed
+- Fixed Windows build
+
+## [9.0.08] - 2026-01-17
+
+### Fixed
+- Fixed Linux build issues
+- Fixed header-only build
+- Added ThorsMug and ThorsSlack to header-only variant
+
+## [9.0.01] - 2026-01-15
+
+### Changed
+- Major refactor of header files
+- Added Nisse (coroutine-based async HTTP server)
+- Added NisseHTTP and NisseServer components
+- Added ThorsMug (hot-loadable plugin server)
+- Added ThorsSlack (Slack REST/webhook client)
+
+## [8.0.01] - 2025-12-15
+
+### Changed
+- Modified numeric values of named logging levels
--- conanfile.py
+++ conanfile.py
@@ -4,14 +4,14 @@ from conan.tools.gnu import Autotools
 from conan.tools.gnu import AutotoolsDeps
 from conan.tools.gnu import AutotoolsToolchain
 
-class ThorsSerializerConan(ConanFile):
+class ThorsAnvilConan(ConanFile):
     name = "thors-anvil"
-    version = "9.0.01"
-    license = "GPL-3.0"
+    version = "9.1.11"
+    license = "MIT"
     author = "Martin York Loki.Astari@gmail.com"
-    url = "https://github.com/Loki-Astari/ThorsSerializer"
-    description = "Serialization Library including C++ Mongo layer"
-    topics = ("C++", "Serialization", "Mongo", "JSON", "BSON", "YAML")
+    url = "https://github.com/Loki-Astari/ThorsAnvil"
+    description = "C++20 monorepo: declarative serialization (JSON/YAML/BSON), async IO, coroutine HTTP server, MongoDB client, Slack client, and hot-loadable plugin framework"
+    topics = ("C++", "Serialization", "Mongo", "JSON", "BSON", "YAML", "HTTP", "Async", "Coroutines")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": True, "fPIC": True}
@@ -33,6 +33,8 @@ class ThorsSerializerConan(ConanFile):
         tc.configure_args.append("--with-crypto-root=" + self.dependencies["openssl"].package_folder)
         print("Magic:   " + self.dependencies["magic_enum"].package_folder)
         tc.configure_args.append("--with-magicenum-header-only-root=" + self.dependencies["magic_enum"].package_folder + "/include/magic_enum")
+        print("Event:   " + self.dependencies["libevent"].package_folder)
+        tc.configure_args.append("--with-event-root=" + self.dependencies["libevent"].package_folder)
         tc.generate()
 
     def requirements(self):
@@ -40,12 +42,14 @@ class ThorsSerializerConan(ConanFile):
         self.requires("zlib/1.3.1")
         self.requires("libyaml/0.2.5")
         self.requires("snappy/1.2.1")
-        self.requires("openssl/3.3.1")
-        self.requires("magic_enum/0.9.6")
+        self.requires("openssl/3.4.1")
+        self.requires("magic_enum/0.9.7")
+        self.requires("boost/1.86.0")
+        self.requires("libevent/2.1.12")
 
     def source(self):
         git = Git(self)
-        git.clone(url="https://github.com/Loki-Astari/ThorsSerializer.git", target=".")
+        git.clone(url="https://github.com/Loki-Astari/ThorsAnvil.git", target=".")
 
     def validate(self):
         tools.build.check_min_cppstd(self, 20)
@@ -60,21 +64,23 @@ class ThorsSerializerConan(ConanFile):
         autotools.install()
 
     def package_info(self):
-        self.cpp_info.libs = [  "ThorSerialize",    "ThorsLogging",    "ThorsMongo",    "ThorsSocket",    "ThorsStorage",       # Release Sym
-                                "ThorSerializeD",   "ThorsLoggingD",   "ThorsMongoD",   "ThorsSocketD",   "ThorsStorageD",      # Debug   Sym
-                                "ThorSerialize20",  "ThorsLogging20",  "ThorsMongo20",  "ThorsSocket20",  "ThorsStorage20",     # Relase Versioned
-                                "ThorSerialize20D", "ThorsLogging20D", "ThorsMongo20D", "ThorsSocket20D", "ThorsStorage20D"     # Debug  Versioned
+        self.cpp_info.libs = [  "ThorSerialize",    "ThorsLogging",    "ThorsMongo",    "ThorsSocket",    "ThorsStorage",    "Nisse",       # Release Sym
+                                "ThorSerializeD",   "ThorsLoggingD",   "ThorsMongoD",   "ThorsSocketD",   "ThorsStorageD",   "NisseD",      # Debug   Sym
+                                "ThorSerialize20",  "ThorsLogging20",  "ThorsMongo20",  "ThorsSocket20",  "ThorsStorage20",  "Nisse20",     # Release Versioned
+                                "ThorSerialize20D", "ThorsLogging20D", "ThorsMongo20D", "ThorsSocket20D", "ThorsStorage20D", "Nisse20D"     # Debug  Versioned
                              ]
         self.cpp_info.libdirs = ["lib"]
         self.cpp_info.includedirs = [ "include",
-                                      "include/GitUtility",
                                       "include/ThorSerialize",
                                       "include/ThorsCrypto",
                                       "include/ThorsIOUtil",
                                       "include/ThorsLogging",
                                       "include/ThorsMongo",
                                       "include/ThorsSocket",
-                                      "include/ThorsStorage"
+                                      "include/ThorsStorage",
+                                      "include/NisseHTTP",
+                                      "include/NisseServer",
+                                      "include/ThorsMug",
+                                      "include/ThorsSlack",
+                                      "include/fast_float"
                                     ]
-
-
